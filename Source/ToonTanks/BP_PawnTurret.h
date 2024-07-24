a00c33c238f76f007799c0b93e4b228e84b384d7
Rafał Lukawski <rs.lukawski@gmail.com>
2024-07-24 05:34:03 +0200

    copy project

---
+++ Source/ToonTanks/BP_PawnTurret.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "BP_PawnTurret.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ABP_PawnTurret : public ABasePawn
+{
+	GENERATED_BODY()
+	
+};
